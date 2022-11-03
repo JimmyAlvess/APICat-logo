ae57ff288d01f1b6ea8ce3e0eaed2e4c05ba5976
Matheus Alves <matheus.180960@gmail.com>
2022-11-03 15:29:22 -0300

    Ajustando as propriedades de modelo com DataAnnotations, criando Migration e populando tabela

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,9 +1,26 @@
-﻿namespace APICatalogo.Models
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
-        public  int CategoriaId { get; set; }
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+        [Key]
+        public int CategoriaId { get; set; }
+        [Required]
+        [MaxLength(80)]
         public string? Nome { get; set; }
-        public string?  ImagemUrl { get; set; }
+        [Required]
+        [MaxLength(300)]
+        public string? ImagemUrl { get; set; }
+
+        public ICollection<Produto> Produtos { get; set; }
     }
 }
+
