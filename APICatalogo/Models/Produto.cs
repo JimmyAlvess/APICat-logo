213de2f89f98e433a39404e331b8f6f2376fa974
Matheus Alves <matheus.180960@gmail.com>
2022-11-03 14:53:21 -0300

    Criando class de modelo

---
+++ APICatalogo/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿namespace APICatalogo.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public string? Preco { get; set; }
+        public string? ImagemUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastroe { get; set; }
+        public Categoria? Categoria { get; set; }
+        public int CategoriaId { get; set; }
+    }
+}
