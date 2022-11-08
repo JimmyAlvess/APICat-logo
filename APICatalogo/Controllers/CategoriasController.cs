8777e9b7078df61e8186be3df65248608eadd13c
Matheus Alves <matheus.180960@gmail.com>
2022-11-08 11:01:48 -0300

    Tratamento de erros usando o bloco try-cath

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -7,76 +7,58 @@ namespace APICatalogo.Controllers
 {
     [Route("api/[Controller]")]
     [ApiController]
-    public class ProdutosController : ControllerBase
+    public class CategoriasController : ControllerBase
     {
-        private readonly AppDbContext _context;
-        public ProdutosController(AppDbContext contexto)
+        public readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext contexto)
         {
             _context = contexto;
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Produto>> Get()
+        public ActionResult<IEnumerable<Categoria>> Get() 
         {
             try
             {
-                return _context.Produtos.AsNoTracking().ToList();
+                return _context.Categorias.AsNoTracking().ToList();
+
             }
             catch (Exception)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, "Error ao tentar obter as categoiras do banco de dados");
             }
-            
         }
-        [HttpGet("{id}",Name = "ObterProduto")]
-        public ActionResult<Produto> GetId(int id)
+
+        [HttpGet("{id}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
         {
             try
             {
-                var produto = _context.Produtos.AsNoTracking()
-              .FirstOrDefault(p => p.ProdutoId == id);
-                if (produto == null)
+                var categorias = _context.Categorias.AsNoTracking()
+                .FirstOrDefault(c => c.CategoriaId == id);
+
+                if (categorias == null)
                 {
-                    return NotFound($"Não foi possível encontrar o produtoId = {id}");
+                    return NotFound($"A categoria com id={id} não foi encontrada");
                 }
-                return produto;
+                return (categorias);
             }
             catch (Exception)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, "Error ao tentar obter as categoiras do banco de dados");
             }
-          
         }
 
         [HttpPost]
-        public ActionResult Post([FromBody]Produto produto)
+        public ActionResult<Categoria> Put([FromBody] Categoria categoria)
         {
             try
             {
-                _context.Produtos.Add(produto);
+                _context.Categorias.Add(categoria);
                 _context.SaveChanges();
                 return new CreatedAtRouteResult("ObterProduto",
-                    new { id = produto.ProdutoId }, produto);
-            }
-            catch (Exception)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, "Error ao tentar obter as categoiras do banco de dados");
-            }
-            
-        }
-        [HttpPut("{id}")]
-        public ActionResult Put(int id,[FromBody]Produto produto)
-        {
-            try
-            {
-                if (id != produto.ProdutoId)
-                {
-                    return BadRequest($"Não foi possível atualizar o produto de id {id}");
-                }
-
-                _context.Entry(produto).State = EntityState.Modified;
-                _context.SaveChanges();
-                return Ok();
+                       new { id = categoria.CategoriaId }, categoria);
             }
             catch (Exception)
             {
@@ -84,28 +66,45 @@ namespace APICatalogo.Controllers
             }
            
         }
-        [HttpDelete("{id}")]
-        public ActionResult<Produto> Delete(int id)
+        [HttpPut("{id}")]
+        public ActionResult<Categoria> Put(int id,[FromBody] Categoria categoria)
         {
             try
             {
-                var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-                //var produto = _context.Produtos.Find(id);
-
-                if (produto == null)
+                if (id != categoria.CategoriaId)
                 {
-                    return NotFound($"Não foi possível encontrar o produto de id {id}");
+                    return BadRequest($"Não foi possível atualizar a categoria com id= {id}");
                 }
 
-                _context.Produtos.Remove(produto);
+                _context.Entry(categoria).State = EntityState.Modified;
                 _context.SaveChanges();
-                return produto;
+                return Ok($"Categoria com id{id} foi atualizada com sucesso");
             }
             catch (Exception)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, "Error ao tentar obter as categoiras do banco de dados");
             }
           
+        }
+        [HttpDelete("{id}")]
+        public ActionResult<Categoria> Delete(int id)
+        {
+            try
+            {
+                var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+                if (categoria == null)
+                {
+                    return NotFound($"A categoria com id {id} não foi encontrada");
+                }
+                _context.Categorias.Remove(categoria);
+                _context.SaveChanges();
+                return categoria;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error ao tentar obter as categoiras do banco de dados");
+            }
+           
         }
     }
 }
