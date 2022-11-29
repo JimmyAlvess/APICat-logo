650438ab8d606d71ab422f03bc46ab16b02c0ddf
Matheus Alves <matheus.180960@gmail.com>
2022-11-29 07:22:18 -0300

    Implementando o padrão Unit Of Work

--- APICatalogo/Repository/IProdutoRepository.cs
+++ APICatalogo/Repository/IProdutoRepository.cs
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-﻿using APICatalogo.Models;
+using APICatalogo.Models;
 
 namespace APICatalogo.Repository
 {
     public interface IProdutoRepository : IRepository<Produto>
     {
         IEnumerable<Produto> GetProdutosPreco();
-=======
-﻿namespace APICatalogo.Repository
-{
-    public interface IProdutoRepository
-    {
->>>>>>> efcac92997fb0368873cebe92508688ccd31aaf2
     }
 }
--- APICatalogo/Repository/IRepository.cs
+++ APICatalogo/Repository/IRepository.cs
@@ -1,20 +1,14 @@
-<<<<<<< HEAD
-﻿using System.Linq.Expressions;
+
+using System.Linq.Expressions;
 
 namespace APICatalogo.Repository
 {
     public interface IRepository<T>
     {
         IQueryable<T> Get();
-        T GetById(Expression<Func<T,bool>> predicate);
+        T GetById(Expression<Func<T, bool>> predicate);
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
-=======
-﻿namespace APICatalogo.Repository
-{
-    public interface IRepository
-    {
->>>>>>> efcac92997fb0368873cebe92508688ccd31aaf2
     }
 }
